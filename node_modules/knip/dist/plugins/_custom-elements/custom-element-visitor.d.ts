f2c39de91894b2d10bd92e682e43665c185c6c6f
Srinivas Sathwik Maddali <sathwikmaddali1233@gmail.com>
2026-06-29 07:34:46 +0530

    remove node_modules from repository

--- node_modules/knip/dist/plugins/_custom-elements/custom-element-visitor.d.ts
+++
@@ -1,7 +0,0 @@
-import type { PluginVisitorContext, PluginVisitorObject } from '../../types/config.ts';
-interface CustomElementVisitorOptions {
-    decoratorName?: string;
-    baseClassName?: string;
-}
-export declare function createCustomElementVisitor(ctx: PluginVisitorContext, isRegistrationSpecifier: (specifier: string) => boolean, { decoratorName, baseClassName }?: CustomElementVisitorOptions): PluginVisitorObject;
-export {};
